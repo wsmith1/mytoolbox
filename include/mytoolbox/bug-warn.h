be60ab14748723509b3730586976f7a4923c6739
William Smith <wsmith1@mail.ru>
2013-04-20 11:13:20 -0400

    bug-warn.h: modify to run in user-space applications

--- include/mytoolbox/bug-warn.h
+++ include/mytoolbox/bug-warn.h
@@ -1,5 +1,7 @@
-#ifndef _PERF_ASM_GENERIC_BUG_H
-#define _PERF_ASM_GENERIC_BUG_H
+#ifndef _MYTOOLBOX_BUG_WARN_H
+#define _MYTOOLBOX_BUG_WARN_H
+#include <stdio.h>
+#include "mytoolbox/compiler.h"
 
 #define __WARN_printf(arg...)	do { fprintf(stderr, arg); } while (0)
 
@@ -8,6 +10,23 @@
 	if (unlikely(__ret_warn_on))		\
 		__WARN_printf(format);		\
 	unlikely(__ret_warn_on);		\
+})
+
+#define WARN_ON(condition) ({						\
+	int __ret_warn_on = !!(condition);				\
+	if (unlikely(__ret_warn_on))					\
+		__WARN_printf("WARNING: '%s'\n", #condition);						\
+	unlikely(__ret_warn_on);					\
+})
+
+#define WARN_ON_ONCE(condition)	({	\
+	static int __warned;			\
+	int __ret_warn_once = !!(condition);	\
+						\
+	if (unlikely(__ret_warn_once))		\
+		if (WARN(!__warned, "WARNING: '%s'\n", #condition)) 	\
+			__warned = 1;		\
+	unlikely(__ret_warn_once);		\
 })
 
 #define WARN_ONCE(condition, format...)	({	\
@@ -19,4 +38,5 @@
 			__warned = 1;		\
 	unlikely(__ret_warn_once);		\
 })
-#endif
+#endif /* _MYTOOLBOX_BUG_WARN_H */
+
