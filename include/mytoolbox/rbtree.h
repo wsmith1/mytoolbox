a2dffad9e0ed75b02e03bfe808e7a195f7a69384
William Smith <wsmith1@mail.ru>
2013-04-20 01:22:19 -0400

    rbtree: modify to run in user-space applications

--- include/mytoolbox/rbtree.h
+++ include/mytoolbox/rbtree.h
@@ -91,21 +91,22 @@ static inline struct page * rb_insert_page_cache(struct inode * inode,
 -----------------------------------------------------------------------
 */
 
-#ifndef	_LINUX_RBTREE_H
-#define	_LINUX_RBTREE_H
+#ifndef	_MYTOOLBOX_RBTREE_H
+#define	_MYTOOLBOX_RBTREE_H
 
-#include <linux/kernel.h>
-#include <linux/stddef.h>
+#define __need_NULL
+#include <stddef.h>
+#include "mytoolbox/macros.h"
+
+#define	RB_RED		0
+#define	RB_BLACK	1
 
 struct rb_node
 {
 	unsigned long  rb_parent_color;
-#define	RB_RED		0
-#define	RB_BLACK	1
 	struct rb_node *rb_right;
 	struct rb_node *rb_left;
-} __attribute__((aligned(sizeof(long))));
-    /* The alignment might seem pointless, but allegedly CRIS needs it */
+};
 
 struct rb_root
 {
@@ -113,24 +114,25 @@ struct rb_root
 };
 
 
-#define rb_parent(r)   ((struct rb_node *)((r)->rb_parent_color & ~3))
+#define rb_parent(r)   ((struct rb_node *)((r)->rb_parent_color & ~3UL))
 #define rb_color(r)   ((r)->rb_parent_color & 1)
 #define rb_is_red(r)   (!rb_color(r))
 #define rb_is_black(r) rb_color(r)
-#define rb_set_red(r)  do { (r)->rb_parent_color &= ~1; } while (0)
-#define rb_set_black(r)  do { (r)->rb_parent_color |= 1; } while (0)
+#define rb_set_red(r)  do { (r)->rb_parent_color &= ~1UL; } while (0)
+#define rb_set_black(r)  do { (r)->rb_parent_color |= 1UL; } while (0)
 
 static inline void rb_set_parent(struct rb_node *rb, struct rb_node *p)
 {
-	rb->rb_parent_color = (rb->rb_parent_color & 3) | (unsigned long)p;
+	rb->rb_parent_color = (rb->rb_parent_color & 3UL) | (unsigned long)p;
 }
 static inline void rb_set_color(struct rb_node *rb, int color)
 {
-	rb->rb_parent_color = (rb->rb_parent_color & ~1) | color;
+	rb->rb_parent_color = (rb->rb_parent_color & ~1UL) | (unsigned long)color;
 }
 
-#define RB_ROOT	(struct rb_root) { NULL, }
+#define RB_ROOT	(struct rb_root) { .rb_node = NULL }
 #define	rb_entry(ptr, type, member) container_of(ptr, type, member)
+#define	rb_entry_qual(ptr, type, member, qual) container_of_qual(ptr, type, member, qual)
 
 #define RB_EMPTY_ROOT(root)	((root)->rb_node == NULL)
 #define RB_EMPTY_NODE(node)	(rb_parent(node) == node)
@@ -174,4 +176,5 @@ static inline void rb_link_node(struct rb_node * node, struct rb_node * parent,
 	*rb_link = node;
 }
 
-#endif	/* _LINUX_RBTREE_H */
+#endif	/* _MYTOOLBOX_RBTREE_H */
+
