aebef15accc5c5ef96bda0819e01b95a3e1ad8a9
William Smith <wsmith1@mail.ru>
2013-05-01 20:42:05 -0400

    list_sort.h: mark functions as extern "C"

--- include/mytoolbox/list_sort.h
+++ include/mytoolbox/list_sort.h
@@ -3,10 +3,18 @@
 
 struct list_head;
 
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
 void list_sort(void *priv, struct list_head *head,
 	       int (*cmp)(void *priv, const struct list_head *a,
 			  const struct list_head *b));
 
 int list_sort_test(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* _MYTOOLBOX_LIST_SORT_H */
